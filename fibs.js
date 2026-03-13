ae58f66e52e4bb0f3861b2ffcc6d780954310988
Ne_Uchi <drmor107@gmail.com>
2026-03-13 18:43:54 +0500

    added fibsRec

--- fibs.js
+++ fibs.js
@@ -6,4 +6,15 @@ function fibs(n) {
   }
   return arr;
 }
-module.exports = fibs;
+
+function fibsRec(n) {
+  if (n === 0) return [0];
+  if (n === 1) return [0, 1];
+
+  let arr = fibsRec(n - 1);
+  arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+
+  return arr;
+}
+
+module.exports = { fibs, fibsRec };
--- fibs.test.js
+++ fibs.test.js
@@ -1,4 +1,4 @@
-const fibs = require('./fibs');
+const { fibs, fibsRec } = require('./fibs');
 
 it('fibonacci iteration test 1', () => {
   expect(fibs(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
@@ -8,4 +8,10 @@ it('fibonacci iteration test 2', () => {
 });
 it('fibonacci iteration test 3', () => {
   expect(fibs(10)).toEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
+});
+it('fibonacci rec test 1', () => {
+  expect(fibsRec(5)).toEqual([0, 1, 1, 2, 3, 5]);
+});
+it('fibonacci rec test 2', () => {
+  expect(fibs(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
 });
